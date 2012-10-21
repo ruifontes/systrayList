72e4847fa30bf2d64e24dbb6f3e68a5b92394beb
Mesar Hameed <mhameed@src.gnome.org>
2012-10-21 10:26:06 +0100

    fix typo in translatable message, suport --> support.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -9,7 +9,7 @@ DONATIONS_URL = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=S
 def onInstall():
 	manifest = addonHandler.getCodeAddon().manifest
 	message = _(""" {name} is a free add-on for NVDA.
-You can make a donation to its author to suport further developments of this add-on and other free software products.
+You can make a donation to its author to support further developments of this add-on and other free software products.
 Do you want to donate now? (you will be redirected to the Paypal website).""").format(**manifest)
 	if gui.messageBox(message, caption=_("Request for Contributions to {name}").format(name=manifest['name']),
 		style=wx.YES_NO|wx.ICON_QUESTION) == wx.YES:
