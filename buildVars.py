7d4d84fc4932383912cabb5240165a2873437dc0
Mesar Hameed <mhameed@src.gnome.org>
2013-01-06 12:35:26 +0100

    Updated to use latest addon-template. individual manifest files will be deleted as the new po files come in.

--- buildVars.py
+++ buildVars.py
@@ -1,6 +1,26 @@
-
 # Build customizations
-# Change this file instead of sconstruct, whenever possible.
+# Change this file instead of sconstruct or manifest files, whenever possible.
+
+# Full getext (please don't change)
+_ = lambda x : x
+
+# Add-on information variables
+addon_info = {
+	# add-on Name
+	"addon-name" : "systrayList",
+	# Add-on description
+	# TRANSLATORS: Summary for this add-on to be shown on installation and add-on informaiton.
+	"addon-summary" : _("List of SysTray and task bar elements"),
+	# Add-on description
+	# Translators: Long description to be shown for this add-on on installation and add-on information
+	"addon-description" : _("""Shows the list of buttons on the System Tray with NVDA+F11 once, twice shows running task lists."""),
+	# version
+	"addon-version" : "1.4",
+	# Author(s)
+	"addon-author" : "Rui Fontes <rui.fontes@tiflotecnia.com>, Rui Batista <ruiandrebatista@gmail.com>, NVDA Community Contributors",
+	# URL for the add-on documentation support
+	"addon-url" : None
+}
 
 import os.path
 
@@ -9,5 +29,5 @@ import os.path
 pythonSources = [os.path.join("addon", "globalPlugins", "systrayList", "*.py"), os.path.join("addon", "installTasks.py")]
 
 # Files that contain strings for translation. Usually your python sources
-i18nSources = pythonSources 
+i18nSources = pythonSources + ["buildVars.py"]
 
