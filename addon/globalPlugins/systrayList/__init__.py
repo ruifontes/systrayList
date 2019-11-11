bf87a29434cde0d17cc071fa639f3a878f873b42
Joseph Lee <joseph.lee22590@gmail.com>
2019-11-10 23:39:07 -0800

    Remove call to unicode() function as it does not exist in python 3.

--- addon/globalPlugins/systrayList/__init__.py
+++ addon/globalPlugins/systrayList/__init__.py
@@ -32,7 +32,7 @@ def mouseEvents(location, *events):
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-	scriptCategory = unicode(_addonSummary) if sys.version_info.major == 2 else _addonSummary
+	scriptCategory = _addonSummary
 
 	def __init__(self):
 		super(GlobalPlugin, self).__init__()
